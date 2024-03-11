6490fe6cca18918e0b587d670448d5e1fe610f12
Rick Gibbed <rick@gibbed.us>
2024-03-11 11:05:44 -0500

    Add Platform enum to Common.

---
+++ projects/Gibbed.DragonsDogma2.Common/Platform.cs
@@ -0,0 +1,46 @@
+﻿/* Copyright (c) 2024 Rick (rick 'at' gibbed 'dot' us)
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+namespace Gibbed.DragonsDogma2.Common
+{
+    // via.CoreEntry.DevelopPlatform
+    public enum Platform
+    {
+        X64 = 1,
+        PS4 = 2,
+        XB1 = 3,
+        UWP = 4,
+        NSW = 5,
+        STM = 6,
+        WGM = 7,
+        PS5 = 8,
+        NIX = 9,
+        STADIA = 10,
+        Android = 11,
+        iOS = 12,
+        UBT = 13,
+        MSG = 14,
+        XBS = 15,
+        SOL = 16,
+        Mac = 17,
+    }
+}
